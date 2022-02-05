2b9f4fbc7243d7db27d468e7e8f701ccc6baca34
Leo Shklovskii <leo@thermopylae.net>
2022-02-05 18:00:25 +0000

    de-ansiblizing

--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
-pip install pipx
-pipx install ansible-base
-pipx inject ansible-base ansible
-pipx ensurepath
-~/.local/bin/ansible localhost -m include_role -a name=linux/personal_config
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto" 2> /dev/null || (cd "${ZDOTDIR:-$HOME}/.zprezto" ; git pull)
+
+ln -sf "${ZDOTDIR:-$HOME}/.zprezto/zlogin" $HOME/.zlogin
+ln -sf "${ZDOTDIR:-$HOME}/.zprezto/zshenv" $HOME/.zshenv
+
+BASEDIR=$(dirname $0)
+
+ln -sf ${BASEDIR}/linux/personal_config/files/p10k.zsh.sh ${HOME}/.p10k.zsh
+ln -sf ${BASEDIR}/linux/personal_config/files/zpreztorc.sh ${HOME}/.zpreztorc
+ln -sf ${BASEDIR}/linux/personal_config/files/zshrc.sh ${HOME}/.zshrc
+
+chsh --shell /usr/bin/zsh
+
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+git config --global pull.rebase true
+git config --global push.default current
